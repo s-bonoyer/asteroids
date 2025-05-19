83cd66d0e1660e94050fb7bcd9a55b5dd1b86ef9
supernov_a <alch3my28@gmail.com>
2025-05-19 14:45:41 -0400

    updated main, added .gitignore

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
-        screen.fill(0,0,0)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
         pygame.display.flip()
 
 if __name__ == "__main__":
