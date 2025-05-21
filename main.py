80ad1bd0ae35a3f3dcd89117d9172cab84d928ca
supernov_a <alch3my28@gmail.com>
2025-05-21 19:57:45 -0400

    created player, edited all files

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import *
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,9 +15,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)
         dt /= 1000
+        
+    
 if __name__ == "__main__":
     main()
