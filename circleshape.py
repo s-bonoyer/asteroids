0f4ebbaf60b9d085f1a88580f922da82039c5391
supernov_a <alch3my28@gmail.com>
2025-05-22 19:13:37 -0400

    created asteroid, asteroidfield, basic functions for them

--- circleshape.py
+++ circleshape.py
@@ -1,4 +1,4 @@
-import pygame, main
+import pygame
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
@@ -11,11 +13,15 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
-    
+    asteroidfield = AsteroidField()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
