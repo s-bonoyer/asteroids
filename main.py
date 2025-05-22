2367b4950c547462b47bff2e53babd2e18bfd9d6
supernov_a <alch3my28@gmail.com>
2025-05-21 22:42:02 -0400

    updated all files, completed most player movement mechanics

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import pygame
 from constants import *
 from player import *
 
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,9 +23,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        for object in updateable:
+            object.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)
         dt /= 1000
